4ab4d422192d5a583ed338a019bcb37b81b5425d
mats cronqvist <mats.cronqvist@gmail.com>
2010-11-06 20:53:00 +0100

    added multi-table capabilities

--- src/velocity.erl
+++ src/velocity.erl
@@ -7,11 +7,10 @@
 -module('velocity').
 -author('mats cronqvist').
 
--export([cfg/0, cfg/1, cfg/2
-         , insert/2
-         , find/2
-         , delete/0
-         , delete/2]).
+-export([cfg/1, cfg/2, cfg/3
+         , delete/1,delete/3
+         , find/3
+         , insert/3]).
 
 %% A schema-less, multi-keyed dictionary of fixed temporal size.
 %%
@@ -20,93 +19,105 @@
 %% no primary key and no schema. Each insert can be of unique form.
 %% Each insert will remove all entries that are "too old", i.e. older
 %% than the timeout. The timeout is settable through the cfg function,
-%% e.g. velocity:cfg(timeout,2) sets the timeout to 2 hours.
+%% e.g. velocity:cfg(Tab,timeout,2) sets the timeout to 2 hours.
 %%
-%% velocity:insert takes two arguments, both of which are a list of
-%% tagged tuples. The elements of the first list are indexed, the
-%% elements of the second list are not. E.g. calling
+%% In addition to the table ID, velocity:insert takes two arguments,
+%% both of which are a list of tagged tuples. The elements of the
+%% first list are indexed, the elements of the second list are
+%% not. E.g. calling
 %%
-%% velocity:insert([{a,"A"},{b,"B"}],[{c,"C"},{d,"D"}]).
+%% velocity:insert(Tab,[{a,"A"},{b,"B"}],[{c,"C"},{d,"D"}]).
 %%
-%% will allow you to find this entry by calling velocity:find(a,"A")
-%% or velocity:find(b,"B").
+%% will allow you to find this entry by calling velocity:find(Tab,a,"A")
+%% or velocity:find(Tab,b,"B").
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% config
 
 default(timeout) -> 1. %hours
 
-cfg(Tag,Val) -> ets:insert(?MODULE,{{cfg,Tag},Val}).
+%% set a cfg
+cfg(Tab,Tag,Val) -> 
+  assert_table(Tab),
+  ets:insert(Tab,{{cfg,Tag},Val}).
 
-cfg(Tag) -> ets:lookup_element(?MODULE,{cfg,Tag},2).
+%% query a cfg
+cfg(Tab,Tag) ->
+  assert_table(Tab),
+  ets:lookup_element(Tab,{cfg,Tag},2).
 
-cfg() -> [{T,V} || [T,V] <- ets:match(?MODULE,{{cfg,'$1'},'$2'})].
+%% list all cfg's
+cfg(Tab) -> 
+  assert_table(Tab),
+  [{T,V} || [T,V] <- ets:match(Tab,{{cfg,'$1'},'$2'})].
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% API
 
 %% insert an entry and its indices, and prune the table
-insert(Indices,Data) ->
-  assert_table(),
-  prune(),
+insert(Tab,Indices,Data) ->
+  assert_table(Tab),
+  prune(Tab),
   TS = now(),
-  [ets:insert(?MODULE,{{Tag,Ind,TS}}) || {Tag,Ind} <- Indices],
-  ets:insert(?MODULE,{TS,Indices,Data}).
+  [ets:insert(Tab,{{Tag,Ind,TS}}) || {Tag,Ind} <- Indices],
+  ets:insert(Tab,{TS,Indices,Data}).
 
 % find an entry by an index and delete it (and its indices)
-delete(Tag,Val) ->
-  assert_table(),
-  case find_ts(Tag,Val) of
+delete(Tab,Tag,Val) ->
+  assert_table(Tab),
+  case find_ts(Tab,Tag,Val) of
     [] -> true;
-    TS -> delete(TS)
+    TS -> delete(Tab,TS)
   end.
 
 % delete an entry (and its indices)
-delete(TS) ->
-  Indices = ets:lookup_element(?MODULE,TS,2),
-  [ets:delete(?MODULE,{T,V,TS}) || {T,V} <- Indices],
-  ets:delete(?MODULE,TS).
+delete(Tab,TS) ->
+  assert_table(Tab),
+  Indices = ets:lookup_element(Tab,TS,2),
+  [ets:delete(Tab,{T,V,TS}) || {T,V} <- Indices],
+  ets:delete(Tab,TS).
 
 % delete the table
-delete() ->
-  catch ets:info(?MODULE,owner) ! quit.
+delete(Tab) ->
+  catch ets:info(Tab,owner) ! quit.
 
 % find an entry by an index
-find(Tag,Val) ->
-  assert_table(),
-  [lookup(TS) || TS <- find_ts(Tag,Val)].
+find(Tab,Tag,Val) ->
+  assert_table(Tab),
+  [lookup(Tab,TS) || TS <- find_ts(Tab,Tag,Val)].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% internal
 
-assert_table() ->
-  case ets:info(?MODULE,owner) of
-   undefined -> mk_table();
+assert_table(Tab) ->
+  case ets:info(Tab,owner) of
+   undefined -> mk_table(Tab);
     _ -> ok
   end.
 
-mk_table() ->
-  {Pid,Ref} = spawn_monitor(mk_table(self())),
+mk_table(Tab) ->
+  {Pid,Ref} = spawn_monitor(mk_table(Tab,self())),
   receive
     {ok,Pid} -> erlang:demonitor(Ref,[flush]);
-    {'DOWN',Ref,_,_,Info} -> exit({error_creating,?MODULE,Info})
+    {'DOWN',Ref,_,_,Info} -> exit({error_creating,Tab,Info})
   end.
   
-mk_table(Daddy) ->
+mk_table(Tab,Daddy) ->
   fun() ->
-      ets:new(?MODULE,[named_table,ordered_set,public]),
-      cfg(timeout,default(timeout)),
+      catch ets:delete(Tab),
+      ets:new(Tab,[named_table,ordered_set,public]),
+      cfg(Tab,timeout,default(timeout)),
       Daddy ! {ok,self()},
       receive quit -> ok end
   end.
 
-find_ts(Tag,Val) ->
-  [TS || [TS] <- ets:match(?MODULE,{{Tag,Val,'$1'}})].
+find_ts(Tab,Tag,Val) ->
+  [TS || [TS] <- ets:match(Tab,{{Tag,Val,'$1'}})].
 
 %delete all entries that have timed out, i.e. are older than N hours.
-prune() ->
-  Then = mk_then(cfg(timeout)),
-  TSs = ets:select(?MODULE,[{{'$1','_','_'},[{'<','$1',{Then}}],['$1']}]),
-  length([delete(TS) || TS <- TSs]).
+prune(Tab) ->
+  Then = mk_then(cfg(Tab,timeout)),
+  TSs = ets:select(Tab,[{{'$1','_','_'},[{'<','$1',{Then}}],['$1']}]),
+  length([delete(Tab,TS) || TS <- TSs]).
 
 %return now() minus N hours
 mk_then(N) -> 
@@ -116,6 +127,6 @@ mk_then(N) ->
 mk_then({MS,S,_},Diff) when Diff=<S -> {MS,S-Diff,0};
 mk_then({MS,S,_},Diff) -> mk_then({MS-1,S+1000000,0},Diff).
 
-lookup(TS) ->
-  [{TS,I,D}] = ets:lookup(?MODULE,TS),
+lookup(Tab,TS) ->
+  [{TS,I,D}] = ets:lookup(Tab,TS),
   I++D.
